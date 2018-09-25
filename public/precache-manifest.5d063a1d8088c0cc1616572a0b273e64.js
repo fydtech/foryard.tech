8f33b07c50b2919877c8a9b1555a4667189e4b30
Sander de Vos <sander@tutanota.de>
2018-09-25 11:37:01 +0200

    FORYARD.tech frontend v1.0.0-rc1

---
+++ public/precache-manifest.5d063a1d8088c0cc1616572a0b273e64.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "255ef43ea306afae15f3147d3fd4677c",
+    "url": "/fonts/ibm-plex-mono-v3-latin-italic.255ef43e.eot"
+  },
+  {
+    "revision": "b94ace1ffd5a7d78ea2e395ab617d5e0",
+    "url": "/img/screenshots/onzezon.png"
+  },
+  {
+    "revision": "fdcb50c9a402610d24e2",
+    "url": "/js/chunk-vendors.b330ac67.js"
+  },
+  {
+    "revision": "183bff089e5371c3a06bdf3d69b1f8e3",
+    "url": "/img/screenshots/stupas.png"
+  },
+  {
+    "revision": "b62e905e01fc25c9181863df4b4a93f0",
+    "url": "/img/vue.b62e905e.svg"
+  },
+  {
+    "revision": "70573366f5b3570812a1e9a5632f8485",
+    "url": "/img/screenshots/stem.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "88788f0bbfc15e553a3d55b55c5681cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d99eab0a296e6a7f07f8c4a13c84b4af",
+    "url": "/img/twitter.d99eab0a.svg"
+  },
+  {
+    "revision": "bf3f75b79ced1a23c60a",
+    "url": "/js/app.a9032c6d.js"
+  },
+  {
+    "revision": "84e539a317f2faa0ccf27998fea473be",
+    "url": "/img/hind-v8-latin-regular.84e539a3.svg"
+  },
+  {
+    "revision": "09cd9139f5b3e235f4883559585885d6",
+    "url": "/img/icon-flame.09cd9139.svg"
+  },
+  {
+    "revision": "9d7cf2845ad99169a6bbf2f21f7e5d17",
+    "url": "/img/laravel.9d7cf284.svg"
+  },
+  {
+    "revision": "833d0520c854b47c4b3cc218cb810fbd",
+    "url": "/img/screenshots/chimp.png"
+  },
+  {
+    "revision": "df90720b72357e92bd257140b28f06cd",
+    "url": "/img/screenshots/buitenlandportaal.png"
+  },
+  {
+    "revision": "f4a15cf85f4b9ca5791aa9d382e0cd8a",
+    "url": "/img/location.f4a15cf8.svg"
+  },
+  {
+    "revision": "37e5a6ab8868e40ed278bf2c618df8a7",
+    "url": "/img/montserrat-v12-latin-300.37e5a6ab.svg"
+  },
+  {
+    "revision": "3f167b47ccbc00d60ce08497cd1a4623",
+    "url": "/img/montserrat-v12-latin-800.3f167b47.svg"
+  },
+  {
+    "revision": "4181f8ac5da2cd814e5af68cb977b256",
+    "url": "/img/phone.4181f8ac.svg"
+  },
+  {
+    "revision": "2e4a0286ced1516e75cb00451046bfa3",
+    "url": "/img/MacBook-Gold.2e4a0286.png"
+  },
+  {
+    "revision": "f9f91b59f0a79360d9676befad0d365d",
+    "url": "/fonts/hind-v8-latin-regular.f9f91b59.eot"
+  },
+  {
+    "revision": "aaddd0148a7c6f9b5a75b92d73fe4bcd",
+    "url": "/fonts/ibm-plex-mono-v3-latin-italic.aaddd014.woff"
+  },
+  {
+    "revision": "c56d2b8b191dba7aa7a1ed61bcc7f850",
+    "url": "/fonts/montserrat-v12-latin-800.c56d2b8b.eot"
+  },
+  {
+    "revision": "b9a72dc227b4a5a28595d946c78aa9b5",
+    "url": "/fonts/ibm-plex-mono-v3-latin-italic.b9a72dc2.ttf"
+  },
+  {
+    "revision": "8e7e467ca654d970db89cdb6e2b2f3d6",
+    "url": "/fonts/ibm-plex-mono-v3-latin-italic.8e7e467c.woff2"
+  },
+  {
+    "revision": "483fc3a532abeb412bb2e6a678d560de",
+    "url": "/fonts/montserrat-v12-latin-300.483fc3a5.ttf"
+  },
+  {
+    "revision": "03873dabf43a99a349dba1bc0ff940c7",
+    "url": "/fonts/montserrat-v12-latin-800.03873dab.ttf"
+  },
+  {
+    "revision": "0a7c6df06e85d978d096d4d18fd8d43d",
+    "url": "/fonts/montserrat-v12-latin-300.0a7c6df0.woff2"
+  },
+  {
+    "revision": "756655905d91b77960888262e7d58d35",
+    "url": "/fonts/montserrat-v12-latin-800.75665590.woff"
+  },
+  {
+    "revision": "5e86df2cad22d2ef2b03516334afae5e",
+    "url": "/fonts/montserrat-v12-latin-300.5e86df2c.woff"
+  },
+  {
+    "revision": "d54cd0fb812144dbcdec5245d168a908",
+    "url": "/img/ibm-plex-mono-v3-latin-italic.d54cd0fb.svg"
+  },
+  {
+    "revision": "fbc6d21dbdfe7281419aefb43c2dac8a",
+    "url": "/fonts/montserrat-v12-latin-300.fbc6d21d.eot"
+  },
+  {
+    "revision": "ffe97e572eda106faf4e82f7645c9c8d",
+    "url": "/img/chevron-down.ffe97e57.svg"
+  },
+  {
+    "revision": "066867387b1cc9e11b1b81c23e8341cd",
+    "url": "/img/email.06686738.svg"
+  },
+  {
+    "revision": "35386154b78d046218fc8f88a44ff515",
+    "url": "/fonts/montserrat-v12-latin-800.35386154.woff2"
+  },
+  {
+    "revision": "e79c78aab446144f374955253415720d",
+    "url": "/img/foryard-logo-tech.e79c78aa.svg"
+  },
+  {
+    "revision": "7f51d1bb861e6439f73c04bcd508ee36",
+    "url": "/img/header-bg.7f51d1bb.jpg"
+  },
+  {
+    "revision": "0dc38941280fdc768a8cc81d15879942",
+    "url": "/img/github.0dc38941.svg"
+  },
+  {
+    "revision": "5161560f6407fe0e424f1cc2cf4c8acb",
+    "url": "/img/chevron-next.5161560f.svg"
+  },
+  {
+    "revision": "c82f3ceb0b74a48f348e2179547e422e",
+    "url": "/img/chevron-prev.c82f3ceb.svg"
+  },
+  {
+    "revision": "92322c7a9c2f7cf79adadcdc7eebd6c0",
+    "url": "/fonts/hind-v8-latin-regular.92322c7a.woff2"
+  },
+  {
+    "revision": "84641d0ba01936df199e2968b2f14e93",
+    "url": "/fonts/hind-v8-latin-regular.84641d0b.woff"
+  },
+  {
+    "revision": "5e2c13d7c2513c85c6fa4e63f862695e",
+    "url": "/fonts/hind-v8-latin-regular.5e2c13d7.ttf"
+  },
+  {
+    "revision": "bf3f75b79ced1a23c60a",
+    "url": "/css/app.460dc286.css"
+  }
+];
